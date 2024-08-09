66f64b7c81f186d44c24dc9c86fb97a0175d751d
akram chawki <btebet@10.0.0.6>
2024-08-09 19:43:15 +0100

    fixed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -48,7 +48,7 @@ class Kernel extends HttpKernel
 
         'auth' => [
         \App\Http\Middleware\Authenticate::class,
-        \App\Http\Middleware\TimeBasedAuth::class, 
+        // \App\Http\Middleware\TimeBasedAuth::class, 
     ],
     ];
 
@@ -70,6 +70,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'time.auth' => \App\Http\Middleware\TimeBasedAuth::class,
+        // 'time.auth' => \App\Http\Middleware\TimeBasedAuth::class,
     ];
 }
