9c1f1e6875673c746abe6f51a2cb2e701ce07ecb
AkramChawki <akramchawki01@gmail.com>
2024-08-12 10:42:55 +0100

    fixed auth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -70,6 +70,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        // 'time.auth' => \App\Http\Middleware\TimeBasedAuth::class,
+        'time.auth' => \App\Http\Middleware\TimeBasedAuth::class,
     ];
 }
