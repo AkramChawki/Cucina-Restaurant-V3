a7b3145a0209e0f4400312ab71a6af2a661c7a81
AkramChawki <akramchawki01@gmail.com>
2025-01-14 19:23:13 +0100

    fix

--- app/Http/Controllers/CoastCuisineController.php
+++ app/Http/Controllers/CoastCuisineController.php
@@ -68,7 +68,7 @@ class CoastCuisineController extends Controller
                 ]);
             }
         }
-
+        dd($categories);
         return Inertia::render('FluxReel/CoastCuisine/CoastCuisineRestau', [
             'restaurant' => $restaurant,
             'categories' => $categories,
